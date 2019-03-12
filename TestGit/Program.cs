97e56eccae9e5955377fcfd3221429c1ac076a60
Pavel_Silukov <434sp89@gmail.com>
2019-03-12 21:43:29 +0300

    Console was added

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -11,6 +11,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!!!!");
+            Console.WriteLine("Privet Andrew");
             Console.ReadKey();
         }
     }
